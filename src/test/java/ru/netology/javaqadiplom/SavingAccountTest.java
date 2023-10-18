3c0445d9c0611b093ac5bf056301d4d1cdb98930
Mikhail <mikle0240@gmail.com>
2023-10-18 22:01:12 +0300

    Modified SavingAccountTest.java

--- src/test/java/ru/netology/javaqadiplom/SavingAccountTest.java
+++ src/test/java/ru/netology/javaqadiplom/SavingAccountTest.java
@@ -127,7 +127,7 @@ public class SavingAccountTest {
 
         account.add(3_000);
 
-        Assertions.assertEquals(2_000 + 3_000, account.getBalance());
+        Assertions.assertEquals(5_000 + 3_000, account.getBalance());
     }
 
     @Test
@@ -141,7 +141,7 @@ public class SavingAccountTest {
 
         account.add(7_000);
 
-        Assertions.assertEquals(10_000, account.getBalance());
+        Assertions.assertEquals(5_000, account.getBalance());
     }
 
     @Test
